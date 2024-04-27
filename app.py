17f58a3bab78d89077e33d930c5933445d197490
botniedata <botniedata@gmail.com>
2024-04-27 08:33:06 +0800

    revised csv file in product folder

--- app.py
+++ app.py
@@ -2,13 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-items = []
 
 @app.get("/")
-def root():
-    return {"hello": "world!"}
-
-@app.post("/items")
-def create_item(item: str):
-    items.append(items)
-    return items
+async def root():
+    return {"message": "Hello World"}
--- extract.py
+++ extract.py
@@ -1,20 +1,18 @@
 import pandas as pd
 import os
 
-# Create a list of data
 products = [
-    [10, 1000.0, "2018-10-01 10:10:10"],
-    [10, 1000.0, "2018-10-01 10:15:10"],
-    [20, 2000.0, "2018-10-01 10:15:20"],
-    [10, 1000.0, "2018-10-01 10:10:10"],
-    [30, 3000.0, "2018-10-01 10:20:10"],
-    [620, 2000.0, "2018-10-01 10:15:30"],
+    [10, "P1", "C1"],
+    [10, "P1", "C1"],
+    [20, "P2", "C2"],
+    [10, "P1", "C1"],
+    [30, "P3", "C3"],
+    [20, "P2", "C2"]
 ]
 
 # Define the column names
-HeaderNames = ["productId", "transactionAmount", "transactionDatetime"]
+HeaderNames = ["productId", "productName", "productManufacturingCity"]
 
-# Create a pandas DataFrame from the data
 df = pd.DataFrame(products, columns=HeaderNames)
 
 # Create the data folder if it doesn't exist
@@ -23,6 +21,6 @@ if not os.path.exists(folderDir):
     os.makedirs(folderDir)
 
 # Save the DataFrame to a CSV file inside the data folder
-df.to_csv(os.path.join(folderDir, "products.csv"), index=False)
+df.to_csv(os.path.join(folderDir, "ProductReference.csv"), index=False)
 
 print("CSV file created successfully in the data directory!")
